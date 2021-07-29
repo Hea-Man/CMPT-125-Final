31cfc7804feace9ed9cbb7050cf9f0c9d3659ab1
Hemant Dhokia <hemantdhokia@live.com>
2021-07-29 00:02:23 -0700

    some more changes

--- main.cpp
+++ main.cpp
@@ -3,10 +3,9 @@ using namespace std;
 
 int main(void)
 {
-    cout << "Hope this works";
+    cout << "Hope this works\n";
 
     int v;
     v = 60+9;
     cout << v << " V variable" << endl;
-    
 }
